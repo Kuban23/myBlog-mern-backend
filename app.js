2bde04b59d3e3387a61955de4c40abb26be8e657
Esin Andrew <kubanesin@gmail.com>
2023-04-24 20:18:19 +0300

    feat-back: заюзал jwt

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
 
-const app= express();
+const app = express();
 
-app.get('/',(req, res)=>{
+app.get('/', (req, res) => {
    res.send('Привет!')
 });
 
-app.listen(4444, (err)=>{
-   if(err){
+app.listen(4444, (err) => {
+   if (err) {
       return console.log(err)
    }
    console.log('Server OK')
+});
+
+app.use(express.json())
+
+app.post('/auth/login', (req, res) => {
+   console.log(req.body)
+   const token = jwt.sign(
+      {
+         email: req.body.email,
+         fullName: 'Вася Пупкин'
+      },
+      'secret123'
+   )
+   res.json(
+      {
+         "saccess": "true",
+          token,
+      }
+   )
 });
